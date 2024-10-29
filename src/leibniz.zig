5876c0f5a4ae7d5e990b2fb453e353adafb13d76
Fabjan Sukalia <fsukalia@gmail.com>
2024-10-29 12:46:00 +0100

    repair zig version

--- src/leibniz.zig
+++ src/leibniz.zig
@@ -4,7 +4,7 @@ pub fn main() anyerror!void {
     var file = try std.fs.cwd().openFile("rounds.txt", .{});
     defer file.close();
     var buffer: [1024]u8 = undefined;
-    var n = try file.read(buffer[0..buffer.len]);
+    const n = try file.readAll(buffer[0..buffer.len]);
     var rounds = try std.fmt.parseInt(i64, std.mem.trim(u8, buffer[0..n], "\n"), 10);
     rounds += 2;
 
@@ -13,7 +13,9 @@ pub fn main() anyerror!void {
     var pi: f64 = 1.0;
     while (i < rounds) : (i += 1) {
         x = -x;
-        pi += (x / @intToFloat(f64, 2 * i - 1));
+        //pi += (x / @intToFloat(f64, 2 * i - 1));
+        const div:f64 = @floatFromInt(2*i-1);
+        pi += (x / div);
     }
     pi *= 4;
     try std.io.getStdOut().writer().print("{}", .{pi});
